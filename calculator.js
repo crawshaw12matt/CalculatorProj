320891c3fe3da2d23be01eed378adc8e01b30958
Matt Crawshaw <crawshaw12matt@gmail.com>
2020-01-27 16:55:57 -0500

    Started adding JS Functionality

--- calculator.js
+++ calculator.js
@@ -6,35 +6,24 @@ function updateDisplay(value){
     document.getElementById('result').innerHTML = value;
 }
 
-document.onload = function(){
+window.onload = function(){
     updateDisplay(currentResult.join(''));
 };
 
-// Handling of "Numbers"
-let pressedNumber = document.getElementsByClassName('number');
-
-pressedNumber.onclick = function() {
-    pressedNumber.style.backgroundColor = "Gainsboro";
-
+// Handling of Buttons
+document.getElementsByClassName("button").onclick = function(){
+    //document.getElementById('7').style.backgroundColor = "Gainsboro";
+    updateDisplay(5);
     // If the current displayed value is 0, just set the currentResult to the pressed number
-    if (currentResult[0] == 0) {
-        currentResult[0] = pressedNumber.innerHTML
-    } else {
-        currentResult.push(pressedNumber.innerHTML)
-    }
-    pressedNumber.style.backgroundColor = "GhostWhite";
-    updateDisplay(currentResult.join(''))
+    //if (currentResult[0] == 0) {
+    //    currentResult[0] = pressedNumber.innerHTML
+    //} else {
+    //    currentResult.push(pressedNumber.innerHTML)
+    //}
+    //document.getElementById('7').style.backgroundColor = "lightgray";
+    //updateDisplay(currentResult.join(''))
 };
 
 // Handling of "Number-functions"
-let pressedFunction = document.getElementsByClassName('function');
-
-pressedFunction.onclick = function(){
-    pressedFunction.style.backgroundColor = "Gainsboro";
-    saveNumber = currentResult;
-    currentResult
-    updateDisplay(currentResult.join(''));
-    pressedFunction.style.backgroundColor = "GhostWhite";
-}
 
 // Handling of "Uses" and "Equals"
